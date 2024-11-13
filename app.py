1fea05a5ab7761981590b00d739b7b48126c355d
Filip Rokita <filiprokita4@gmail.com>
2024-11-13 13:21:35 +0000

    Add main function to app.py for article processing

--- app.py
+++ app.py
@@ -2,3 +2,10 @@ import openai
 
 # Set API key
 openai.api = "TEST123123"
+
+def main():
+    article_text = "Lorem Ipsum" # TODO: Load article from file
+    html_content = "Lorem Ipsum" # TODO: Generate HTML content from article
+
+if __name__ == "__main__":
+    main()
