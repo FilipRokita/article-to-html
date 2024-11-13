a309c0cd4e41ef82b010be328cbe2bcfda754303
Filip Rokita <filiprokita4@gmail.com>
2024-11-13 13:38:30 +0000

    Changed spacing

--- app.py
+++ app.py
@@ -8,10 +8,12 @@ def main():
     article_text = "Lorem Ipsum" # TODO: Load article from file
     html_content = "Lorem Ipsum" # TODO: Generate HTML content from article
 
+
 # Define function to load article from file
 def load_article(filename):
     with open(filename, 'r') as file:
         return file.read()
+
 
 # Start the program if this script is run directly
 if __name__ == "__main__":
